f0bff0884a089c651402575087fc5c2c6406c5e8
Christopher Schott <christopher.schott@kupferwerk.com>
2015-12-04 21:41:04 +0100

    integrate neopixel

--- python_modules/led_server.py
+++ python_modules/led_server.py
@@ -1,20 +1,45 @@
 '''
    Simple socket server using threads
 '''
-
+import neopixel as np
 import socket
 import sys
 import signal
 from thread import *
 
+# Server-Settings
 HOST = 'localhost'   # Symbolic name meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
-EXPECTED_DATA_LENGTH = 3 * 60
+
+# LED Properties
+LED_COUNT = 60
+LED_COUNT      = 60      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+# Buffering: TODO This is not Threadsafe.
+EXPECTED_DATA_LENGTH = 3 * LED_COUNT
 BUFFER = ''
 
 def handle_frame(frame):
-    for s in frame:
-        print int(s.encode('hex'), 16)
+    red = 0
+    green = 0
+    blue = 0
+    for index, value in enumerate(frame):
+        colorIndex = index % 3
+        stripIndex = int(index / 3)
+        colorValue =  int(value.encode('hex'), 16)
+        if colorIndex == 0:
+            red = colorValue
+        elif colorIndex == 1:
+            green = colorValue
+        else:
+            blue = colorValue
+            strip.setPixelColor(stripIndex, red, green, blue)
+    strip.show()
 
 def set_colors(socket_data):
     global BUFFER
@@ -30,6 +55,11 @@ def set_colors(socket_data):
         handle_frame(frame)
 
 def start_socket():
+    # Create NeoPixel object with appropriate configuration.
+    strip = np.Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print 'Socket created'
 
